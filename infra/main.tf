62f5f3b35bad38943fe226759a8beb67f5dda075
Goekan Malkoc <goekan.malkoc@gmail.com>
2025-04-25 14:26:37 +0200

    Change main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.27.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 
+#   subscription_id = var.subscription_id
+#   tenant_id       = var.tenant_id
+#   client_id       = var.client_id
+#   client_secret   = var.client_secret
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -8,31 +21,32 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
   name                = "flask-asp"
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
-  
-  kind     = "Linux"        # Bunu ekle
-  reserved = true # Linux için
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
 }
 
-resource "azurerm_app_service" "app" {
+resource "azurerm_linux_web_app" "app" {
   name                = "flask-terraform-app"
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
-    linux_fx_version = "PYTHON|3.10"
+    always_on = false
+      application_stack {
+    python_version = "3.12"
+  }
   }
 
+
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "MY_SETTING" = "some-value"
   }
 }
+
+
